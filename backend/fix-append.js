b77af195a6e1758a795670c2ef06289a0f8a5fe0
Mayank <mayankjha8274@gmail.com>
2026-05-12 17:33:02 +0530

    chore: production-only cleanup

--- backend/fix-append.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const file = 'src/controllers/append.js';
-const data = fs.readFileSync(file, 'utf16le');
-fs.writeFileSync(file, data, 'utf8');
-console.log('Fixed append.js');
